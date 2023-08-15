5c2d83561f29a07f9337f6f8c66b7695d134914b
mathematiger <mcop.dkoehler@gmail.com>
2023-08-15 08:35:39 +0200

    update

--- fds.py
+++ fds.py
@@ -37,13 +37,12 @@ else:
     data = torch.load(path+'fds'+str(num_layers_gnn))
 
 
-
 #train GNN on data
 path = 'content_fds/models/'
 
 
 if num_layers_gnn == 4:
-    model = GNN_GCN_4(in_channels=2, hidden_channels=32, out_channels=data.num_classes, num_layers = num_layers_gnn)
+    model = GNN_GCN_4(in_channels=2, hidden_channels=64, out_channels=data.num_classes, num_layers = num_layers_gnn)
     optimizer = torch.optim.Adam(model.parameters(), lr = 0.001, weight_decay = 0.001)
     criterion = torch.nn.CrossEntropyLoss()
     if new_dataset == True:
@@ -53,7 +52,7 @@ if num_layers_gnn == 4:
     else:
         model.load_state_dict(torch.load(path+'model'+str(num_layers_gnn)))
 elif num_layers_gnn == 3:
-    model = GNN_GCN_3(in_channels=2, hidden_channels=32, out_channels=data.num_classes, num_layers = num_layers_gnn)
+    model = GNN_GCN_3(in_channels=2, hidden_channels=64, out_channels=data.num_classes, num_layers = num_layers_gnn)
     optimizer = torch.optim.Adam(model.parameters(), lr = 0.001, weight_decay = 0.001)
     criterion = torch.nn.CrossEntropyLoss()
     if new_dataset == True:
@@ -63,11 +62,11 @@ elif num_layers_gnn == 3:
     else:
         model.load_state_dict(torch.load(path+'model'+str(num_layers_gnn)))
 elif num_layers_gnn == 2:
-    model = GNN_GCN_2(in_channels=2, hidden_channels=32, out_channels=data.num_classes, num_layers = num_layers_gnn)
-    optimizer = torch.optim.Adam(model.parameters(), lr = 0.001, weight_decay = 0.001)
+    model = GNN_GCN_2(in_channels=2, hidden_channels=64, out_channels=data.num_classes, num_layers = num_layers_gnn)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.002, weight_decay = 0.00015)
     criterion = torch.nn.CrossEntropyLoss()
     if new_dataset == True:
-        for _ in range(500):
+        for _ in range(900):
             loss = train(model, optimizer, criterion, data)
         torch.save(model.state_dict(), path+'model'+str(num_layers_gnn))
     else:
@@ -76,19 +75,13 @@ elif num_layers_gnn == 2:
 
 
 
-model = GIN(input_feat = 2, hidden_channels = 32, classes = data.num_classes)
-for _ in range(500):
-    loss = train(model, optimizer, criterion, data)
-
-
-node_idx = choose_xnode(data, label_to_explain)
-
-
-# Testing
+#  Testing
 model.eval()
 out = model(data.x, data.edge_index)
 pred = out.argmax(dim=1)  # Use the class with highest probability.
 true_Y = data.y[data.test_mask].tolist()
+for _ in range(len(true_Y)):
+    print(true_Y[_], pred[data.test_mask].tolist()[_])
 acc = accuracy_score(true_Y, pred[data.test_mask].tolist())
 #acc, f1, prec, rec, auprc, auroc = test(model, data, num_classes = data.num_classes, get_auc = False)
 print('Test Accuracy score GCN: {:.4f}'.format(acc))
@@ -101,7 +94,7 @@ print('Different classes to explain: ', data.num_classes)
 print('Layers of the GNN: ', num_layers_gnn)
 
 # choose label to explain
-node_idx = choose_xnode(data, label_to_explain)
+node_idx = choose_xnode(data, label_to_explain, pred)
 pred = model(data.x, data.edge_index)[node_idx,:].argmax(dim=0)
 
 label = data.y[node_idx].item()
--- fds_data.py
+++ fds_data.py
@@ -7,60 +7,53 @@ import numpy as np
 import torch
 from torch_geometric.utils import from_networkx
 from tqdm import tqdm as tq
-from torch_geometric.nn import HeteroConv, SAGEConv, Linear, GCNConv, GINConv
+from torch_geometric.nn import HeteroConv, SAGEConv, Linear, GCNConv, GINConv, GCN
 from graphxai.explainers import *
 
 device = None
 
 
-
+# -- adjusted from https://github.com/m30m/gnn-explainability/blob/main/benchmarks/infection.py
 def create_dataset(max_dist):
     #max_dist = self.num_layers  # anything larger than max_dist has a far away label
-    g = nx.erdos_renyi_graph(500, 0.004, directed=True)
-    N = len(g.nodes())
-    infected_nodes = rnd.sample(g.nodes(), 150)
-    g.add_node('X')  # dummy node for easier computation, will be removed in the end
-    for u in infected_nodes:
-        g.add_edge('X', u)
-    shortest_path_length = nx.single_source_shortest_path_length(g, 'X')
-    unique_solution_nodes = []
-    unique_solution_explanations = []
-    labels = []
     number_features = 2
-    features = np.ones((N, number_features))
-    features_list = features.tolist()
-    for _ in range(len(features_list)):
-        features_list[_] = [rnd.random()+0.1 for _ in range(number_features)]
-        if _ in infected_nodes:
-            features_list[_] = [0 for _ in range(number_features)]
-    features = torch.tensor(features_list)
-    for i in range(N):
-        if i == 'X':
-            continue
-        length = shortest_path_length.get(i, 100) - 1  # 100 is inf distance
-        labels.append(min(max_dist + 1, length))
-        col = 0 if i in infected_nodes else 1
-        features[i, col] = 1
-        if 0 < length <= max_dist:
-            path_iterator = iter(nx.all_shortest_paths(g, 'X', i))
-            unique_shortest_path = next(path_iterator)
-            if next(path_iterator, 0) != 0:
-                continue
-            unique_shortest_path.pop(0)  # pop 'X' node
-            if len(unique_shortest_path) == 0:
-                continue
-            unique_solution_explanations.append(unique_shortest_path)
-            unique_solution_nodes.append(i)
-    g.remove_node('X')
+    g = nx.erdos_renyi_graph(1000, 0.004, directed=True)
+    N = len(g.nodes())
+    labels = [0 for _ in range(N)]
+    infected_nodes = rnd.sample(g.nodes(), 80)
+    #alternate way to get node labels
+    if max_dist == 1:
+        for _ in range(N):
+            labels[_] = 2
+        for u in infected_nodes:
+            labels[u] = 0
+        for u in infected_nodes:
+            for _ in g.neighbors(u):
+                #print(58, _)
+                if labels[_] >= 1:
+                    labels[_] = 1
+    if max_dist == 2:
+        for _ in range(N):
+            labels[_] = 3
+        for u in infected_nodes:
+            labels[u] = 0
+        for u in infected_nodes:
+            for _ in g.neighbors(u):
+                if labels[_] >= 1:
+                    labels[_] = 1
+                for __ in g.neighbors(_):
+                    if labels[__] >= 2:
+                        labels[__] = 2
+    features = np.zeros((N, number_features)).tolist()
+    for _ in range(N):
+        if labels[_] == 0:
+            features[_] = [0.0,1.0]
+        else:
+            features[_] = [2.0, 0.0]
+
     data = from_networkx(g)
     data.x = torch.tensor(features, dtype=torch.float)
     data.y = torch.tensor(labels)
-    
-    #data.x = data.y
-    # all infected nodes get feature one:
-    
-    data.unique_solution_nodes = unique_solution_nodes
-    data.unique_solution_explanations = unique_solution_explanations
     data.num_classes = 1 + max_dist + 1
     
     edges_tensor = data.edge_index
@@ -77,46 +70,26 @@ def create_dataset(max_dist):
     sorted_list1, sorted_list2 = zip(*sorted_lists)
     edges_tensor =  torch.tensor([sorted_list1, sorted_list2])
     data.edge_index = edges_tensor
-    '''wrong direction:
-    edges_tensor = data.edge_index
-    pairs_set = set(zip(edges_tensor[0].tolist(), edges_tensor[1].tolist()))
-    sorted_pairs = sorted(pairs_set)
-    edges_tensor = torch.tensor(sorted_pairs)
-    data.edge_index = edges_tensor
-    '''
-    # Convert unique_solution_nodes to a set for faster lookup
-    unique_solution_nodes_set = set(unique_solution_nodes)
-
     # Create train, validation, and test masks
     train_mask = torch.zeros(N, dtype=torch.bool)
     val_mask = torch.zeros(N, dtype=torch.bool)
     test_mask = torch.zeros(N, dtype=torch.bool)
-    '''
-    for i in range(N):
-        if i in unique_solution_nodes_set:
-            if rnd.random() < 0.8:  # 80% of unique_solution_nodes for training
-                train_mask[i] = True
-            else:
-                val_mask[i] = True  # Remaining 20% for validation
-        else:
-            test_mask[i] = True  # Non-unique_solution_nodes for testing
-    '''
-    choose_which_mask = rnd.random()
     for _ in range(N):
-        if choose_which_mask < 0.3:
+        choose_which_mask = rnd.random()
+        if choose_which_mask < 0.4:
             train_mask[_] = True
-        elif choose_which_mask < 0.2:
+        elif choose_which_mask < 0.6:
             val_mask[_] = True
         else:
             test_mask[_] = True
-    for i in range(N):
-        if i in unique_solution_nodes_set:
-            if rnd.random() < 0.8:  # 80% of unique_solution_nodes for training
-                train_mask[i] = True
+        if labels[_] == 0:
+            train_mask[_] = False
+            val_mask[_] = False
+            test_mask[_] = False
+            if choose_which_mask < 0.6:  # 60% of unique_solution_nodes for training
+                train_mask[_] = True
             else:
-                val_mask[i] = True  # Remaining 20% for validation
-        else:
-            test_mask[i] = True  # Non-unique_solution_nodes for testing
+                val_mask[_] = True  # Remaining for validation
     data.train_mask = torch.tensor(train_mask)
     data.val_mask = torch.tensor(val_mask)
     data.test_mask = torch.tensor(test_mask)
@@ -127,7 +100,7 @@ def create_dataset(max_dist):
 
 
 class GNN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+    def __init__(self, in_channels, hidden_channels, out_channels):
         super().__init__()
         self.conv1 = SAGEConv(in_channels, hidden_channels, dropout = 0.1)
         self.conv2 = SAGEConv(hidden_channels, hidden_channels, dropout = 0.1)
@@ -148,11 +121,12 @@ class GNN(torch.nn.Module):
         return x
 
 class GNN_GCN_2(torch.nn.Module):
+    '''
     def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
         super().__init__()
-        self.conv1 = GCNConv(in_channels, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, out_channels)
-        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.conv1 = GCNConv(in_channels, hidden_channels, improved = True, add_selve_loops = True, bias = True, dropout = 0.1)
+        self.conv2 = GCNConv(hidden_channels, out_channels, improved = True, add_selve_loops = True, bias = False, dropout = 0.1)
+        self.conv3 = GCNConv(hidden_channels, out_channels, improved = True, add_selve_loops = True, bias = False, dropout = 0.1)
         self.conv4 = GCNConv(hidden_channels, hidden_channels)
         self.lin = torch.nn.Linear(hidden_channels, out_channels)
     def forward(self, x, edge_index):
@@ -163,7 +137,25 @@ class GNN_GCN_2(torch.nn.Module):
         #x = self.lin(x)
         #x = self.conv3(x)  # Final layer for node classification
         return x
+    '''
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+        super().__init__()
+        self.mlp_gin1 = torch.nn.Linear(in_channels, hidden_channels)
+        self.gin1 = GINConv(self.mlp_gin1)
+        self.mlp_gin2 = torch.nn.Linear(hidden_channels, out_channels)
+        self.gin2 = GINConv(self.mlp_gin2)
+        self.mlp_gin3 = torch.nn.Linear(hidden_channels, out_channels)
+        self.gin3 = GINConv(self.mlp_gin3)
 
+    def forward(self, x, edge_index):
+        # NOTE: our provided testing function assumes no softmax
+        #   output from the forward call.
+        x = self.gin1(x, edge_index)
+        x = x.relu()
+        x = self.gin2(x, edge_index)
+        #x = x.relu()
+        #x = self.conv3(x, edge_index)
+        return x
 
 
 class GNN_GCN_3(torch.nn.Module):
@@ -210,6 +202,32 @@ class GNN_GCN_4(torch.nn.Module):
         #x = self.conv3(x)  # Final layer for node classification
         return x
 
+class GNN_GCN_5(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.conv4 = GCNConv(hidden_channels, hidden_channels)
+        self.conv5 = GCNConv(hidden_channels, out_channels)
+        self.lin = torch.nn.Linear(hidden_channels, out_channels)
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv2(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv3(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv4(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv5(x, edge_index)
+        #x = torch.relu(x)
+        #x = self.lin(x)
+        #x = self.conv3(x)  # Final layer for node classification
+        return x
+
+
+
 
 class GIN(torch.nn.Module):
     def __init__(self,input_feat, hidden_channels, classes = 2):
@@ -233,19 +251,19 @@ class GIN(torch.nn.Module):
 
 
 
-def choose_xnode(data, label):
+def choose_xnode(data, label, pred):
     boolean_tensor = data.test_mask.tolist()
     #true_indices = torch.nonzero(boolean_tensor).squeeze()
     #random_true_index = torch.randint(len(true_indices), size=(1,)).item()
     #list of indices of label label and in test_mask:
     list_of_possible_indices = []
     for _ in range(len(data.y.tolist())):
-        if data.y.tolist()[_] == label and boolean_tensor[_] == True:
+        if pred.tolist()[_] == label and boolean_tensor[_] == True:
             list_of_possible_indices.append(_)
     #selected_index = true_indices[random_true_index]
     random_index = rnd.choice(list_of_possible_indices)
     #return selected_index.item()
-    print(127, 'number of elements in the test-set of label ', label, ' : ', len(list_of_possible_indices))
+    #print(127, 'number of elements in the test-set of label ', label, ' : ', len(data.y.tolist()))
     return random_index
 
 
@@ -333,7 +351,7 @@ def get_exp_method(method, model, criterion, pred_class, data, layers_of_gnn, no
         #acc = graph_exp_acc(gt_exp = gt_exp[0], generated_exp = method_exp)
         feedback = "PGM Explainer"
     elif method=='pgex':
-        exp_method=PGExplainer(model, emb_layer_name = 'conv'+str(layers_of_gnn), max_epochs = 10, lr = 0.1)
+        exp_method=PGExplainer(model, emb_layer_name = 'gin'+str(layers_of_gnn), max_epochs = 10, lr = 0.1)
         forward_kwargs={'node_idx': node_idx,
                         'x': data.x.to(device),
                         'edge_index': data.edge_index.to(device),
